92c40559b480f0e737b704094c97d099216a5366
Smit Patadiya <smit.soni22@gmail.com>
2019-10-09 15:08:20 +0530

    Remove footer

--- footer.php
+++ footer.php
@@ -12,22 +12,7 @@
 ?>
 
 	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'avocado' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'avocado' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'avocado' ), 'avocado', '<a href="https://smitpatadiya.com">Smit Patadiya</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
